7fd1ed7e90477d9f8eb3e1c6fd33a430d0f6724a
jranya <jranya2025@gmail.com>
2026-03-01 12:43:25 +0300

    Added footer folder, installed Bootstrap & Bootstrap Icons

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/header/header';
+import { Footer } from './shared/footer/footer';  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],  
+  template: `
+    <app-header></app-header>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  `,
+  styleUrls: ['./app.css'],
 })
 export class App {
-  protected readonly title = signal('E-commerce-System');
+  protected readonly title = signal('E-Commerce-Template');
 }
